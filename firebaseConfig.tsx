ffa1730b630dd1d4064da5f39f49ff0a7af7a919
hokageCodes <hashsenju94@gmail.com>
2024-08-11 13:15:04 +0100

    fix: bug fixed & done hop

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,12 +1,13 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
-import { getFirestore, Firestore, collection, addDoc, getDocs, getDoc, deleteDoc, doc } from "firebase/firestore";
+import { getFirestore, Firestore, addDoc, collection, deleteDoc, doc, getDoc, getDocs } from "firebase/firestore";
 import { getAuth, Auth, signInWithEmailAndPassword } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { getStorage, FirebaseStorage } from "firebase/storage";
 
+// Declare variables with initial value as undefined
 let app: FirebaseApp | undefined;
 let db: Firestore | undefined;
 let auth: Auth | undefined;
-let storage: ReturnType<typeof getStorage> | undefined;
+let storage: FirebaseStorage | undefined;
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuny5xpmnG3WJ66hPi3RtunJVFXPm1AcM",
@@ -23,12 +24,12 @@ if (typeof window !== "undefined" && !app) {
   app = initializeApp(firebaseConfig);
   db = getFirestore(app);
   auth = getAuth(app);
-  storage = getStorage(app); // Initialize Firebase Storage
+  storage = getStorage(app);
 }
 
-// Ensure that auth and other variables are defined before exporting
-if (!auth || !db || !storage) {
+// Check if Firebase services are initialized before exporting
+if (!app || !db || !auth || !storage) {
   throw new Error("Firebase services are not properly initialized");
 }
 
-export { db, collection, addDoc, getDocs, getDoc, deleteDoc, doc, auth, signInWithEmailAndPassword, storage, firebaseConfig };
+export { db, collection, addDoc, getDocs, getDoc, deleteDoc, doc, auth, signInWithEmailAndPassword, storage };
